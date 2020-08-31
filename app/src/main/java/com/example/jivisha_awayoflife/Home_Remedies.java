fbddf94ba7ead567ec1b3c26e847393cff32e759
arshawalia71 <arshawalia71@gmail.com>
2020-08-31 18:51:55 +0530

    Main page UI

---
+++ app/src/main/java/com/example/jivisha_awayoflife/Home_Remedies.java
@@ -0,0 +1,14 @@
+package com.example.jivisha_awayoflife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_Remedies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__remedies);
+    }
+}
--- app/src/main/java/com/example/jivisha_awayoflife/MainActivity.java
+++ app/src/main/java/com/example/jivisha_awayoflife/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.jivisha_awayoflife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+//    CardView card_view = (CardView) findViewById(R.id.card1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        card_view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               Intent intent=new Intent(MainActivity.this, Doctors.class);
+//               startActivity(intent);
+//            }
+//        });
+    }
+
+    public void button1_click(View view) {
+
+        Intent intent=new Intent(this,Doctors.class);
+        startActivity(intent);
+    }
+
+    public void button2_click(View view) {
+
+        Intent intent=new Intent(this,Home_Remedies.class);
+        startActivity(intent);
+    }
+
+    public void button3_click(View view) {
+
+        Intent intent=new Intent(this,About_You.class);
+        startActivity(intent);
+    }
+
+    public void button4_click(View view) {
+
+        Intent intent=new Intent(this,Lifestyle.class);
+        startActivity(intent);
     }
 }
